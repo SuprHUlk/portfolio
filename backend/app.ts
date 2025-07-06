e4705bf1df33d348ee41c3bd0105f84517ebf0cc
SuprHUlk <ayush123kumar.ak@gmail.com>
2025-07-06 14:49:30 +0530

    fix: refactor activity handling and improve error logging in presence updates

--- backend/app.ts
+++ backend/app.ts
@@ -2,11 +2,11 @@ import "dotenv/config";
 
 import express from "express";
 import {
-  Client,
-  Events,
-  GatewayIntentBits,
-  Presence,
-  RichPresenceAssets,
+    Client,
+    Events,
+    GatewayIntentBits,
+    Presence,
+    RichPresenceAssets,
 } from "discord.js";
 import { createServer } from "node:http";
 import Activity from "./models/Activity";
@@ -23,155 +23,177 @@ app.use(cors());
 const server = createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+    cors: {
+        origin: [
+            "https://www.suprhulk.com",
+            "https://portfolio-suprhulks-projects.vercel.app",
+            "https://portfolio-git-main-suprhulks-projects.vercel.app",
+            "http://localhost:4200",
+        ],
+        methods: ["GET", "POST"],
+    },
 });
 
 const port = process.env.PORT;
 const token = process.env.DISCORD_BOT_TOKEN;
 const userId: string = process.env.USER_ID!;
+const rawgApiKey: string = process.env.RAWG_API_KEY!;
 
 const defaultActivity: Activity = {
-  name: "Sleeping",
-  type: 6,
-  details: "In bed",
-  state: "Zzzz",
-  imageUrl: process.env.DEFAULT_ACTIVITY_IMAGE_URL!,
+    name: "Sleeping",
+    type: 6,
+    details: "In bed",
+    state: "Zzzz",
+    imageUrl: process.env.DEFAULT_ACTIVITY_IMAGE_URL!,
 };
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildPresences,
-  ],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildPresences,
+    ],
 });
 
 async function pollUserPresence(): Promise<Activity | null> {
-  let userPresence: Presence | null = null;
+    let userPresence: Presence | null = null;
 
-  for (const [_, guild] of client.guilds.cache) {
-    const member = await guild.members.fetch(userId).catch(() => null);
-    if (member?.presence) {
-      userPresence = member.presence;
-      break;
+    for (const [_, guild] of client.guilds.cache) {
+        const member = await guild.members.fetch(userId).catch(() => null);
+        if (member?.presence) {
+            userPresence = member.presence;
+            break;
+        }
     }
-  }
 
-  if (userPresence != null && userPresence.activities.length != 0) {
-    return getActivity(userPresence);
-  }
+    if (userPresence != null && userPresence.activities.length != 0) {
+        return await getActivity(userPresence);
+    }
 
-  return null;
+    return null;
 }
 
-function getActivity(presence: Presence): Activity {
-  const curActivity = presence.activities[0];
-  const name = curActivity.name;
-  const type = curActivity.type;
-  const details = curActivity.details;
-  const state = curActivity.state;
-  const imageUrl = getUrl(curActivity.assets);
+async function getActivity(presence: Presence): Promise<Activity | null> {
+    try {
+        const curActivity = presence.activities[0];
+        const name = curActivity.name;
+        const type = curActivity.type;
+        const details = curActivity.details;
+        const state = curActivity.state;
+        const imageUrl = await getUrl(name, curActivity.assets);
 
-  const activity: Activity = {
-    name,
-    type,
-    details,
-    state,
-    imageUrl,
-  };
+        const activity: Activity = {
+            name,
+            type,
+            details,
+            state,
+            imageUrl,
+        };
 
-  return activity;
+        return activity;
+    } catch (err) {
+        logger.error("Error in getActivity", { error: err });
+        return null;
+    }
 }
 
-function getUrl(asset: RichPresenceAssets | null): string {
-  if (asset === null || asset === undefined)
-    return process.env.NOT_FOUND_IMAGE_URL!;
+async function getUrl(
+    name: string,
+    asset: RichPresenceAssets | null
+): Promise<string> {
+    try {
+        if (asset === null || asset === undefined)
+            return (
+                (await getImageFromRawg(name)) ??
+                process.env.NOT_FOUND_IMAGE_URL!
+            );
 
-  const imageUrl =
-    asset.largeImage !== null
-      ? asset.largeImageURL()
-      : asset.smallImage !== null
-      ? asset.smallImageURL()
-      : "";
+        let imageUrl =
+            asset.largeImage !== null
+                ? asset.largeImageURL()
+                : asset.smallImage !== null
+                ? asset.smallImageURL()
+                : await getImageFromRawg(name);
 
-  return imageUrl ?? process.env.NOT_FOUND_IMAGE_URL!;
+        return imageUrl ?? process.env.NOT_FOUND_IMAGE_URL!;
+    } catch (err) {
+        logger.error("Error in getUrl", { error: err });
+        return process.env.NOT_FOUND_IMAGE_URL!;
+    }
+}
+
+async function getImageFromRawg(name: string): Promise<string | null> {
+    try {
+        const apiRes = await fetch(
+            "https://api.rawg.io/api/games?key=" +
+                rawgApiKey +
+                "&search=" +
+                name
+        );
+        const data = await apiRes.json();
+        const res: any[] = data.results;
+
+        if (!res.length || !res[0].background_image) {
+            return null;
+        }
+        return res[0].background_image;
+    } catch (err) {
+        logger.error("Error in getImageFromRawg", { error: err });
+        return null;
+    }
 }
 
 client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    logger.info(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-const listners = new Map<string, Socket>();
-
-client.on("presenceUpdate", (_, newP: Presence | null) => {
-  if (newP?.userId == userId) {
-    if (newP.activities.length != 0) {
-      const activity = getActivity(newP);
-      listners.forEach((listner) => {
-        logger.info(
-          "sending to: " + listner.id + " activity: " + JSON.stringify(activity)
-        );
-        listner.emit("activity", activity);
-      });
-    } else {
-      listners.forEach((listner) => {
-        logger.info(
-          "sending to: " +
-            listner.id +
-            " activity: " +
-            JSON.stringify(defaultActivity)
-        );
-        listner.emit("activity", defaultActivity);
-      });
+client.on("presenceUpdate", async (_, newP: Presence | null) => {
+    if (newP?.userId == userId) {
+        if (newP.activities.length != 0) {
+            const activity = await getActivity(newP);
+            logger.info(activity);
+            io.emit("activity", activity);
+        } else {
+            io.emit("activity", defaultActivity);
+        }
     }
-  }
 });
 
 io.on("connection", (socket) => {
-  logger.info("Connection made: " + socket.id);
-  listners.set(socket.id, socket);
-
-  socket.on("ready", async () => {
-    let activity = await pollUserPresence();
-    logger.info(
-      "sending to: " +
-        socket.id +
-        " activity: " +
-        JSON.stringify(activity == null ? defaultActivity : activity)
-    );
-    socket.emit("activity", activity == null ? defaultActivity : activity);
-  });
-
-  socket.on("disconnect", () => {
-    logger.info("Disconnected: " + socket.id);
-    listners.delete(socket.id);
-  });
+    logger.info("Connection made: " + socket.id);
+    socket.on("ready", async () => {
+        let activity = await pollUserPresence();
+        logger.info("sending to: " + socket.id, {
+            activity: activity ?? defaultActivity,
+        });
+        socket.emit("activity", activity ?? defaultActivity);
+    });
+    socket.on("disconnect", () => {
+        logger.info("Disconnected: " + socket.id);
+    });
 });
 
 client.login(token);
 
 app.get("/getQuote", (req, res) => {
-  const quote = selectQuote(quotes);
-  logger.info("Selected quote: " + JSON.stringify(quote));
-  res.status(200).json(quote);
+    const quote = selectQuote(quotes);
+    logger.info("Selected quote:", { quote: quote });
+    res.status(200).json(quote);
 });
 
 function selectQuote(data: any): Quote {
-  const quote = data[Math.floor(Math.random() * data.length)];
-  if (quote.text.length > 60) {
-    return selectQuote(data);
-  }
-  return quote;
+    const quote = data[Math.floor(Math.random() * data.length)];
+    if (quote.text.length > 60) {
+        return selectQuote(data);
+    }
+    return quote;
 }
 
 //health checkpoint
 app.get("/", (_, res: any) => {
-  res.status(200).send("OK");
+    res.status(200).send("OK");
 });
 
 server.listen(port, () => {
-  logger.info("Listening on port: " + port);
+    logger.info("Listening on port: " + port);
 });
